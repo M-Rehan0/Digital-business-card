2c15e70876319c6d0ced8d62cfc8368721389075
M-Rehan0 <muhammadrehan.chaudh@ucalgary.ca>
2023-07-01 17:57:29 -0600

    Footer update

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,7 +1,27 @@
 import React from "react";
+import { FaGithub, FaLinkedin } from "react-icons/fa";
 
 function Footer() {
-  return
+  return (
+    <div className="socials">
+      <a
+        id="github-icon"
+        href="https://github.com/M-Rehan0"
+        target="_blank"
+        rel="noreferrer"
+      >
+        <FaGithub className="github" />
+      </a>
+      <a
+        id="linkedin-icon"
+        href="https://www.linkedin.com/in/muhammad-rehan-chaudhary/"
+        target="_blank"
+        rel="noreferrer"
+      >
+        <FaLinkedin className="linkedIn" />
+      </a>
+    </div>
+  );
 }
 
 export default Footer;
