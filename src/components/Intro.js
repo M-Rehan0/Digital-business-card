082e19e34927d6a1356f4a02bcea722567a554ed
M-Rehan0 <muhammadrehan.chaudh@ucalgary.ca>
2023-07-01 18:03:25 -0600

    Intro added

--- src/components/Intro.js
+++ src/components/Intro.js
@@ -1,7 +1,29 @@
 import React from "react";
 
 function Intro() {
-  return
+  return (
+    <div>
+      <img id="pic" src="http://surl.li/hdaof" alt="Muhammad Rehan Chaudhary" />
+      <div className="intro">
+        <p className="name">Muhammad Rehan Chaudhary</p>
+        <p>Software Developer</p>
+        <p>Student at University of Calgary</p>
+      </div>
+
+      <div className="buttons">
+        <a
+          href="mailto:muhammadrhn71@gmail.com"
+          target="_blank"
+          rel="noreferrer"
+        >
+          <button id="email-btn">Email</button>
+        </a>
+        <a href="#">
+          <button id="resume-btn">Resume</button>
+        </a>
+      </div>
+    </div>
+  );
 }
 
 export default Intro;
