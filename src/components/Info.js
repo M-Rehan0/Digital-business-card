7fdd86a23b43033c32246c7c9147f60276400de9
M-Rehan0 <muhammadrehan.chaudh@ucalgary.ca>
2023-07-01 18:02:00 -0600

    Info added

--- src/components/Info.js
+++ src/components/Info.js
@@ -1,7 +1,28 @@
 import React from "react";
 
 function Info() {
-  return 
+  return (
+    <div className="info">
+      <p id="info-title-1">About Me</p>
+      <p>
+        Second year Software Engineer student 💻 Always learning new things ✨
+        Project based experience in Fullstack programming 🤓 Let's get coding!
+        🚀
+      </p>
+      <br />
+      <p id="info-title-2">Interests</p>
+      <div id="interest-list">
+        <ul>
+          <li>Reader</li>
+          <li>Tech fanatic</li>
+        </ul>
+        <ul>
+          <li>Business enthusiast</li>
+          <li>Wanderlust</li>
+        </ul>
+      </div>
+    </div>
+  );
 }
 
 export default Info;
